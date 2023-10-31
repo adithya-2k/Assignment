539512ca4d85f571d77d4376501550748a8ae647
Adithya Kumar <adithyak@infinite.com>
2023-10-31 17:55:50 +0530

    Asp Assignment

---
+++ Assignments/ASP/Assignment1Asp/Assignment1Asp/Index.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Assignment1Asp.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="313px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Get Price" />
+            <br />
+            <br />
+        </div>
+        <asp:Image ID="Image1" runat="server" Height="334px" Width="866px" />
+        <br />
+        <br />
+        Price is :&nbsp;&nbsp;
+        <asp:Label ID="Label1" runat="server" Text="--"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Assignments/ASP/Assignment1Asp/Assignment1Asp/Index.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment1Asp
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                string[] val = new string[] { "Select", "Mercedes", "BMW" };
+
+                for (int i = 0; i < val.Length; i++)
+                {
+                    DropDownList1.Items.Add(val[i]);
+                }
+
+            }
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string val = DropDownList1.Text;
+            Image1.ImageUrl = "~/pics/" + val + ".jpg";
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string val = DropDownList1.Text;
+
+            if (DropDownList1.SelectedValue == "BMW")
+            {
+                Label1.Text = "35,00,000";
+            }
+            else if (DropDownList1.SelectedValue == "Mercedes")
+            {
+                Label1.Text = "20,00,000";
+            }
+            else
+            {
+                Label1.Text = "--";
+            }
+        }
+    }
+}
