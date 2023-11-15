e16a3e510d5269666d4bf81267c4a8b8eeade5d3
Adithya Kumar <adithyak@infinite.com>
2023-11-15 21:25:58 +0530

    Web API Assignment

---
+++ Assignments/WebAPI/WebApi/Controllers/CountryController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class CountryController : ApiController
+    {
+
+        static List<Country> cnt = new List<Country>()
+        {
+            new Country{ID=1, CountryName="India", Capital="Delhi"},
+            new Country{ID=2, CountryName="Uinted State", Capital="Washington, D.C."}
+        };
+
+        [HttpGet]
+        [Route("All")]
+        public IEnumerable<Country> Get()
+        {
+            return cnt;
+        }
+
+
+        [HttpPost]
+        [Route("post")]
+        public Country Post([FromBody] Country C)
+        {
+            cnt.Add(C);
+            return C;
+        }
+
+        [HttpPut]
+        [Route("put")]
+        public void Put(int Id, [FromBody] Country C)
+        {
+            cnt[Id - 1] = C;
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public void Delete(int Id, [FromBody] Country C)
+        {
+            cnt.RemoveAt(Id - 1);
+        }
+
+
+    }
+}
---
+++ Assignments/WebAPI/WebApi/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Country
+    {
+        public int ID { get; set; }
+        public string CountryName { get; set; }
+        public string Capital { get; set; }
+
+
+
+
+
+    }
+}
